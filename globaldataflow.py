c0518c6f97486f2e8790622e34b3f33c1e2e5f30
Madhurima Chakraborty <56744323+MadhuNimmo@users.noreply.github.com>
2022-07-20 15:36:59 -0700

    Update globaldataflow.py

--- globaldataflow.py
+++ globaldataflow.py
@@ -1,8 +1,8 @@
-def function A(argA1,argA2):
+def A(argA1,argA2):
   return argA1+"strng"
-def function B(argB1):
+def B(argB1):
   return argB1
 def main():
-  x = A("update")
+  x = A("update","me")
   B(x)
 main()
