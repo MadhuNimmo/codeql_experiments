a5750ce2e491119a001063b6f1b3003bdbfe0172
madhunimmo <mchak009@ucr.edu>
2022-07-26 14:05:06 -0700

    global data flow

--- globaldataflow.py
+++ globaldataflow.py
@@ -1,11 +1,14 @@
 def A(argA1,argA2):
   return argA1+"strng"
 def B(argB1):
-  return argB1
+  print(argB1)
 def concatStr(s1,s2):
     return s1+s2
 def main(argM1,argM2):
-  x = concatStr(argM1,argM2)  
-  y = A(x,"extra")
-  B(y)
+  x = concatStr(argM1,argM2)
+  z = input()
+  y = ""
+  if(z=="y"):
+    y = A(x,"extra")
+  B(y)  
 main("please","update")
